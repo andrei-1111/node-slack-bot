60e1917bcbf773884b41cb7839483333cd0307c7
Andrei Elizaga <naaeliz@gmail.com>
2019-12-10 01:05:31 +0800

    Install slack web-api for sending a request for  slack events. Create initial slack mention event.

--- routes/bots/slack.js
+++ routes/bots/slack.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { createEventAdapter } = require('@slack/events-api');
+const { WebClient } = require('@slack/web-api');
 
 const router = express.Router();
 
@@ -7,8 +8,22 @@ module.exports = (params) => {
   const { config } = params;
 
   const slackEvents = createEventAdapter(config.slack.signingSecret);
+  const slackWebClient = new WebClient(config.slack.token);
 
   router.use('/events', slackEvents.requestListener());
+
+  async function handleMention(event) {
+    const text = 'Example text message.';
+    const username = 'John';
+
+    return slackWebClient.chat.postMessage({
+      text: text,
+      channel: event.channel,
+      username: username,
+    });
+  }
+
+  slackEvents.on('app_mention', handleMention);
 
   return router;
 };
